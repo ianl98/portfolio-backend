249f8c1d3bba4161f30c50c89eaa2b3679afc827
Ian Lindstrom <ianl_98@hotmail.com>
2022-05-14 18:56:27 -0300

    hechas las funcionalidades crud de educacion

--- src/main/java/com/portfolio/argprograma/controllers/EducacionController.java
+++ src/main/java/com/portfolio/argprograma/controllers/EducacionController.java
@@ -1,7 +1,48 @@
 package com.portfolio.argprograma.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.portfolio.argprograma.models.Educacion;
+import com.portfolio.argprograma.services.EducacionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("api/educacion")
 public class EducacionController {
+
+    @Autowired
+    EducacionService educacionService;
+
+    @GetMapping("/all")
+    public List<Educacion> getEducaciones(){
+        return educacionService.getEducaciones();
+    }
+
+    @PostMapping("/save")
+    public String saveEducacion(@RequestBody Educacion educacion){
+
+        educacionService.saveEducacion(educacion);
+
+        return "se ha guardado satisfactoriamente";
+
+    }
+
+    @PutMapping("/edit/{id}")
+    public void updateEducacion(@PathVariable Integer id, @RequestBody Educacion educacion){
+
+        educacionService.updateEducacion(id, educacion);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteEducacion(@PathVariable Integer id){
+        educacionService.deleteEducacion(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Educacion> getOneEducacion(@PathVariable Integer id) {
+        return educacionService.getOneEducacion(id);
+    }
 }
--- src/main/java/com/portfolio/argprograma/services/EducacionService.java
+++ src/main/java/com/portfolio/argprograma/services/EducacionService.java
@@ -1,4 +1,45 @@
 package com.portfolio.argprograma.services;
 
+import com.portfolio.argprograma.models.Educacion;
+import com.portfolio.argprograma.repositorys.EducacionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
 public class EducacionService {
+
+    @Autowired
+    EducacionRepository educacionRepository;
+
+    public List<Educacion> getEducaciones() {
+        return educacionRepository.findAll();
+    }
+
+    public void saveEducacion(Educacion educacion) {
+        educacionRepository.save(educacion);
+    }
+
+    public Educacion updateEducacion(Integer id, Educacion educacion){
+        Optional <Educacion> educacionOptional = educacionRepository.findById(id);
+
+        educacion.setId(educacionOptional.get().getId());
+
+        educacionRepository.save(educacion);
+
+        return educacion;
+    }
+
+    public void deleteEducacion(int id) {
+        educacionRepository.deleteById(id);
+    }
+
+    public Optional<Educacion> getOneEducacion(int id) {
+        return educacionRepository.findById(id);
+    }
 }
