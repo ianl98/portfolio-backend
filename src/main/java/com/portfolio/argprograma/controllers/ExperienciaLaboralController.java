34fc1a22504e93525be52230917027ebe52ce96e
Ian Lindstrom <ianl_98@hotmail.com>
2022-05-14 19:55:27 -0300

    hechas las funcionalidades crud de Experiencia Laboral

--- src/main/java/com/portfolio/argprograma/controllers/ExperienciaLaboralController.java
+++ src/main/java/com/portfolio/argprograma/controllers/ExperienciaLaboralController.java
@@ -1,7 +1,48 @@
 package com.portfolio.argprograma.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+
+import com.portfolio.argprograma.models.ExperienciaLaboral;
+import com.portfolio.argprograma.services.ExperienciaLaboralService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("api/experienciaLaboral")
 public class ExperienciaLaboralController {
+
+    @Autowired
+    ExperienciaLaboralService experienciaLaboralService;
+
+    @GetMapping("/all")
+    public List<ExperienciaLaboral> getExperienciasLaborales(){
+        return experienciaLaboralService.getExperienciasLaborales();
+    }
+
+    @PostMapping("/save")
+    public String saveExperienciaLaboral(@RequestBody ExperienciaLaboral experienciaLaboral){
+
+        experienciaLaboralService.saveExperienciaLaboral(experienciaLaboral);
+
+        return "se ha guardado satisfactoriamente";
+
+    }
+
+    @PutMapping("/edit/{id}")
+    public void updateExperienciaLaboral(@PathVariable Integer id, @RequestBody ExperienciaLaboral experienciaLaboral){
+
+        experienciaLaboralService.updateExperienciaLaboral(id, experienciaLaboral);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteExperienciaLaboral(@PathVariable Integer id){
+        experienciaLaboralService.deleteExperienciaLaboral(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<ExperienciaLaboral> getOneExperienciaLaboral(@PathVariable Integer id) {
+        return experienciaLaboralService.getOneExperienciaLaboral(id);
+    }
 }
--- src/main/java/com/portfolio/argprograma/services/ExperienciaLaboralService.java
+++ src/main/java/com/portfolio/argprograma/services/ExperienciaLaboralService.java
@@ -1,4 +1,45 @@
 package com.portfolio.argprograma.services;
 
+import com.portfolio.argprograma.models.Educacion;
+import com.portfolio.argprograma.models.ExperienciaLaboral;
+import com.portfolio.argprograma.repositorys.ExperienciaLaboralRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
 public class ExperienciaLaboralService {
+
+    @Autowired
+    ExperienciaLaboralRepository experienciaLaboralRepository;
+
+    public List<ExperienciaLaboral> getExperienciasLaborales() {
+        return experienciaLaboralRepository.findAll();
+    }
+
+    public void saveExperienciaLaboral(ExperienciaLaboral experienciaLaboral) {
+        experienciaLaboralRepository.save(experienciaLaboral);
+    }
+
+    public ExperienciaLaboral updateExperienciaLaboral(Integer id, ExperienciaLaboral experienciaLaboral){
+        Optional<ExperienciaLaboral> experienciaLaboralOptional = experienciaLaboralRepository.findById(id);
+
+        experienciaLaboral.setId(experienciaLaboralOptional.get().getId());
+
+        experienciaLaboralRepository.save(experienciaLaboral);
+
+        return experienciaLaboral;
+    }
+
+    public void deleteExperienciaLaboral(int id) {
+        experienciaLaboralRepository.deleteById(id);
+    }
+
+    public Optional<ExperienciaLaboral> getOneExperienciaLaboral(int id) {
+        return experienciaLaboralRepository.findById(id);
+    }
 }
