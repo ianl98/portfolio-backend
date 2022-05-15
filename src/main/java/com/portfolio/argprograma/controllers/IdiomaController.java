2dcfad6e979b99404bb139feaeee3461fb187c7f
Ian Lindstrom <ianl_98@hotmail.com>
2022-05-15 01:22:24 -0300

    hechas las funcionalidades crud de Idioma

--- src/main/java/com/portfolio/argprograma/controllers/IdiomaController.java
+++ src/main/java/com/portfolio/argprograma/controllers/IdiomaController.java
@@ -1,7 +1,47 @@
 package com.portfolio.argprograma.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.portfolio.argprograma.models.Idioma;
+import com.portfolio.argprograma.services.IdiomaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("api/idioma")
 public class IdiomaController {
+
+    @Autowired
+    IdiomaService idiomaService;
+
+    @GetMapping("/all")
+    public List<Idioma> getIdiomas(){
+        return idiomaService.getIdiomas();
+    }
+
+    @PostMapping("/save")
+    public String saveIdioma(@RequestBody Idioma idioma){
+
+        idiomaService.saveIdioma(idioma);
+
+        return "se ha guardado satisfactoriamente";
+
+    }
+
+    @PutMapping("/edit/{id}")
+    public void updateIdioma(@PathVariable Integer id, @RequestBody Idioma idioma){
+
+        idiomaService.updateIdioma(id, idioma);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteIdioma(@PathVariable Integer id){
+        idiomaService.deleteIdioma(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Idioma> getOneIdioma(@PathVariable Integer id) {
+        return idiomaService.getOneIdioma(id);
+    }
 }
--- src/main/java/com/portfolio/argprograma/services/IdiomaService.java
+++ src/main/java/com/portfolio/argprograma/services/IdiomaService.java
@@ -1,4 +1,45 @@
 package com.portfolio.argprograma.services;
 
+
+import com.portfolio.argprograma.models.Idioma;
+import com.portfolio.argprograma.repositorys.IdiomaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
 public class IdiomaService {
+
+    @Autowired
+    IdiomaRepository idiomaRepository;
+
+    public List<Idioma> getIdiomas() {
+        return idiomaRepository.findAll();
+    }
+
+    public void saveIdioma(Idioma idioma) {
+        idiomaRepository.save(idioma);
+    }
+
+    public Idioma updateIdioma(Integer id, Idioma idioma){
+        Optional<Idioma> idiomaOptional = idiomaRepository.findById(id);
+
+        idioma.setId(idiomaOptional.get().getId());
+
+        idiomaRepository.save(idioma);
+
+        return idioma;
+    }
+
+    public void deleteIdioma(int id) {
+        idiomaRepository.deleteById(id);
+    }
+
+    public Optional<Idioma> getOneIdioma(int id) {
+        return idiomaRepository.findById(id);
+    }
 }
