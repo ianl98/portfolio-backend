9d675dfec332133324e6acc579b40d3bea187f0d
Ian Lindstrom <ianl_98@hotmail.com>
2022-05-15 02:03:54 -0300

    hechas las funcionalidades crud de Persona

--- src/main/java/com/portfolio/argprograma/controllers/PersonaController.java
+++ src/main/java/com/portfolio/argprograma/controllers/PersonaController.java
@@ -1,7 +1,47 @@
 package com.portfolio.argprograma.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.portfolio.argprograma.models.Persona;
+import com.portfolio.argprograma.services.PersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("api/persona")
 public class PersonaController {
+
+    @Autowired
+    PersonaService personaService;
+
+    @GetMapping("/all")
+    public List<Persona> getPersonas(){
+        return personaService.getPersonas();
+    }
+
+    @PostMapping("/save")
+    public String savePersona(@RequestBody Persona persona){
+
+        personaService.savePersona(persona);
+
+        return "se ha guardado satisfactoriamente";
+
+    }
+
+    @PutMapping("/edit/{id}")
+    public void updatePersona(@PathVariable Integer id, @RequestBody Persona persona){
+
+        personaService.updatePersona(id, persona);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deletePersona(@PathVariable Integer id){
+        personaService.deletePersona(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Persona> getOnePersona(@PathVariable Integer id) {
+        return personaService.getOnePersona(id);
+    }
 }
--- src/main/java/com/portfolio/argprograma/services/PersonaService.java
+++ src/main/java/com/portfolio/argprograma/services/PersonaService.java
@@ -1,4 +1,44 @@
 package com.portfolio.argprograma.services;
 
+import com.portfolio.argprograma.models.Persona;
+import com.portfolio.argprograma.repositorys.PersonaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
 public class PersonaService {
+
+    @Autowired
+    PersonaRepository personaRepository;
+
+    public List<Persona> getPersonas() {
+        return personaRepository.findAll();
+    }
+
+    public void savePersona(Persona persona) {
+        personaRepository.save(persona);
+    }
+
+    public Persona updatePersona(Integer id, Persona persona){
+        Optional<Persona> personaOptional = personaRepository.findById(id);
+
+        persona.setId(personaOptional.get().getId());
+
+        personaRepository.save(persona);
+
+        return persona;
+    }
+
+    public void deletePersona(int id) {
+        personaRepository.deleteById(id);
+    }
+
+    public Optional<Persona> getOnePersona(int id) {
+        return personaRepository.findById(id);
+    }
 }
