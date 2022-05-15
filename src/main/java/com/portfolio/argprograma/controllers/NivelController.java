42296d3fd493224aa971df25a170d36006d8524c
Ian Lindstrom <ianl_98@hotmail.com>
2022-05-14 22:23:42 -0300

    hechas las funcionalidades crud de Nivel

--- src/main/java/com/portfolio/argprograma/controllers/NivelController.java
+++ src/main/java/com/portfolio/argprograma/controllers/NivelController.java
@@ -1,7 +1,47 @@
 package com.portfolio.argprograma.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.portfolio.argprograma.models.Nivel;
+import com.portfolio.argprograma.services.NivelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("api/nivel")
 public class NivelController {
+
+    @Autowired
+    NivelService nivelService;
+
+    @GetMapping("/all")
+    public List<Nivel> getNiveles(){
+        return nivelService.getNiveles();
+    }
+
+    @PostMapping("/save")
+    public String saveNivel(@RequestBody Nivel nivel){
+
+        nivelService.saveNivel(nivel);
+
+        return "se ha guardado satisfactoriamente";
+
+    }
+
+    @PutMapping("/edit/{id}")
+    public void updateNivel(@PathVariable Integer id, @RequestBody Nivel nivel){
+
+        nivelService.updateNivel(id, nivel);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteNivel(@PathVariable Integer id){
+        nivelService.deleteNivel(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Nivel> getOneNivel(@PathVariable Integer id) {
+        return nivelService.getOneNivel(id);
+    }
 }
--- src/main/java/com/portfolio/argprograma/services/NivelService.java
+++ src/main/java/com/portfolio/argprograma/services/NivelService.java
@@ -1,4 +1,44 @@
 package com.portfolio.argprograma.services;
 
+import com.portfolio.argprograma.models.Nivel;
+import com.portfolio.argprograma.repositorys.NivelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
 public class NivelService {
+
+    @Autowired
+    NivelRepository nivelRepository;
+
+    public List<Nivel> getNiveles() {
+        return nivelRepository.findAll();
+    }
+
+    public void saveNivel(Nivel nivel) {
+        nivelRepository.save(nivel);
+    }
+
+    public Nivel updateNivel(Integer id, Nivel nivel){
+        Optional<Nivel> nivelOptional = nivelRepository.findById(id);
+
+        nivel.setId(nivelOptional.get().getId());
+
+        nivelRepository.save(nivel);
+
+        return nivel;
+    }
+
+    public void deleteNivel(int id) {
+        nivelRepository.deleteById(id);
+    }
+
+    public Optional<Nivel> getOneNivel(int id) {
+        return nivelRepository.findById(id);
+    }
 }
