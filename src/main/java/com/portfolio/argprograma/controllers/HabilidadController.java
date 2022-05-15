c5790c3236eb51b27acda5ff421da3d78291ba42
Ian Lindstrom <ianl_98@hotmail.com>
2022-05-14 21:02:15 -0300

    hechas las funcionalidades crud de Habilidad

--- src/main/java/com/portfolio/argprograma/controllers/HabilidadController.java
+++ src/main/java/com/portfolio/argprograma/controllers/HabilidadController.java
@@ -1,7 +1,48 @@
 package com.portfolio.argprograma.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+
+import com.portfolio.argprograma.models.Habilidad;
+import com.portfolio.argprograma.services.HabilidadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("api/habilidad")
 public class HabilidadController {
+
+    @Autowired
+    HabilidadService habilidadService;
+
+    @GetMapping("/all")
+    public List<Habilidad> getHabilidades(){
+        return habilidadService.getHabilidades();
+    }
+
+    @PostMapping("/save")
+    public String saveHabilidad(@RequestBody Habilidad habilidad){
+
+        habilidadService.saveHabilidad(habilidad);
+
+        return "se ha guardado satisfactoriamente";
+
+    }
+
+    @PutMapping("/edit/{id}")
+    public void updateHabilidad(@PathVariable Integer id, @RequestBody Habilidad habilidad){
+
+        habilidadService.updateHabilidad(id, habilidad);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteHabilidad(@PathVariable Integer id){
+        habilidadService.deleteHabilidad(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Habilidad> getOneHabilidad(@PathVariable Integer id) {
+        return habilidadService.getOneHabilidad(id);
+    }
 }
