e898a909052d0e75a68db4f11143044f192249e9
Ian Lindstrom <ianl_98@hotmail.com>
2022-05-15 23:08:02 -0300

    hechas las funcionalidades crud de Proyecto

--- src/main/java/com/portfolio/argprograma/controllers/ProyectoController.java
+++ src/main/java/com/portfolio/argprograma/controllers/ProyectoController.java
@@ -1,7 +1,48 @@
 package com.portfolio.argprograma.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.portfolio.argprograma.models.Proyecto;
+import com.portfolio.argprograma.services.ProyectoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("api/proyecto")
 public class ProyectoController {
+
+    @Autowired
+    ProyectoService proyectoService;
+
+
+    @GetMapping("/all")
+    public List<Proyecto> getProyectos(){
+        return proyectoService.getProyectos();
+    }
+
+    @PostMapping("/save")
+    public String saveProyecto(@RequestBody Proyecto proyecto){
+
+        proyectoService.saveProyecto(proyecto);
+
+        return "se ha guardado satisfactoriamente";
+
+    }
+
+    @PutMapping("/edit/{id}")
+    public void updateProyecto(@PathVariable Integer id, @RequestBody Proyecto proyecto){
+
+        proyectoService.updateProyecto(id, proyecto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteProyecto(@PathVariable Integer id){
+        proyectoService.deleteProyecto(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Proyecto> getOneProyecto(@PathVariable Integer id) {
+        return proyectoService.getOneProyecto(id);
+    }
 }
--- src/main/java/com/portfolio/argprograma/services/ProyectoService.java
+++ src/main/java/com/portfolio/argprograma/services/ProyectoService.java
@@ -1,4 +1,45 @@
 package com.portfolio.argprograma.services;
 
+import com.portfolio.argprograma.models.Proyecto;
+import com.portfolio.argprograma.repositorys.ProyectoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
 public class ProyectoService {
+
+    @Autowired
+    ProyectoRepository proyectoRepository;
+
+    public List<Proyecto> getProyectos() {
+        return proyectoRepository.findAll();
+    }
+
+    public void saveProyecto(Proyecto proyecto) {
+        proyectoRepository.save(proyecto);
+    }
+
+    public Proyecto updateProyecto(Integer id, Proyecto proyecto){
+        Optional<Proyecto> proyectoOptional = proyectoRepository.findById(id);
+
+        proyecto.setId(proyectoOptional.get().getId());
+
+        proyectoRepository.save(proyecto);
+
+        return proyecto;
+    }
+
+    public void deleteProyecto(int id) {
+        proyectoRepository.deleteById(id);
+    }
+
+    public Optional<Proyecto> getOneProyecto(int id) {
+        return proyectoRepository.findById(id);
+    }
+
 }
