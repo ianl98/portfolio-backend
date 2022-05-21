8400bdc0b4b6e9fb27affc5f3fe249926ddf3067
Ian Lindstrom <ianl_98@hotmail.com>
2022-05-21 20:01:02 -0300

    agregado habilidad a la rama main

--- src/main/java/com/portfolio/argprograma/services/HabilidadService.java
+++ src/main/java/com/portfolio/argprograma/services/HabilidadService.java
@@ -1,4 +1,45 @@
 package com.portfolio.argprograma.services;
 
+import com.portfolio.argprograma.models.Habilidad;
+import com.portfolio.argprograma.repositorys.HabilidadRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
 public class HabilidadService {
+
+    @Autowired
+    HabilidadRepository habilidadRepository;
+
+    public List<Habilidad> getHabilidades() {
+        return habilidadRepository.findAll();
+    }
+
+    public void saveHabilidad(Habilidad habilidad) {
+        habilidadRepository.save(habilidad);
+    }
+
+    public Habilidad updateHabilidad(Integer id, Habilidad habilidad){
+        Optional<Habilidad> habilidadOptional = habilidadRepository.findById(id);
+
+        habilidad.setId(habilidadOptional.get().getId());
+
+        habilidadRepository.save(habilidad);
+
+        return habilidad;
+    }
+
+    public void deleteHabilidad(int id) {
+        habilidadRepository.deleteById(id);
+    }
+
+    public Optional<Habilidad> getOneHabilidad(int id) {
+        return habilidadRepository.findById(id);
+    }
 }
